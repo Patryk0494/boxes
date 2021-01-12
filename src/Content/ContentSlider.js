e047dbc5fc8177189072bb4854cbb62dc0b5a195
Patryk <jpatryk0494@gmail.com>
2021-01-13 00:28:32 +0100

    task almost complete

--- src/Content/ContentSlider.js
+++ src/Content/ContentSlider.js
@@ -22,49 +22,6 @@ const boxData = [
 let data = boxData;
 
 class ContentSlider extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {data: data}
-        this.indexDecreas = this.indexDecreas.bind(this);
-        this.indexIncreas = this.indexIncreas.bind(this);
-    }
-
-
-    indexDecreas() {
-    //     console.log(boxData[0])
-    //     console.log(boxData[1])
-    //     console.log(boxData[2])
-    //     this.state.data.map((element, index, arr) => {
-    //     if (index-1 < 0) {
-    //         arr[index] = this.state.data[this.state.data.length-1]
-    //         return this.setState({data: data[0]=arr[index]})
-    //     } else {
-    //         arr[index] = this.state.data[index-1]
-    //         return this.setState({data: data[index]=arr[index]})
-    //     }
-    // })
-        let { data } = this.state
-        for (let i = 0; i < data.length; i++) {
-            console.log(data)
-            if ( i === 0 ) {
-                return this.setState({data : data[0] = this.state.data[this.state.data.length-1]})
-            } 
-            data[i] = this.state.data[i-1];
-            return this.setState({data: data})
-        }
-        console.log(data);
-    }
-
-
-    indexIncreas() {
-        console.log(boxData[0])
-        boxData.map((element, index) => {
-        if (index+1 > boxData.length) {
-        return element[index] = element[0]
-        }
-        return element[index]=element[index+1]
-    })
-    }
 
     render(){
         return(
